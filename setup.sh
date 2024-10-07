e2e8342e454c434cf22be316b3c5278ec95d178f
paul <paul.plays.a.pun@gmail.com>
2024-10-07 04:43:15 +0000

    added build files

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Configure Git
+git config --global user.name "paul"
+git config --global user.email "paul.plays.a.pun@gmail.com"
+
+# Download and install CMake
+wget https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1-Linux-x86_64.sh
+chmod +x cmake-3.25.1-Linux-x86_64.sh
+sudo ./cmake-3.25.1-Linux-x86_64.sh --skip-license --prefix=/usr/local
+rm -rf cmake-3.25.1-Linux-x86_64.sh
+
+# Update the PATH environment variable
+echo 'export PATH=/usr/local/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+echo "Installation and configuration complete!"
