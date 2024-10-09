27254fe76ac519a4d0c0e79b0b6f19586561981b
paul <paul.plays.a.pun@gmail.com>
2024-10-09 02:06:11 +0000

    shmem kernel added

--- src/sgemm/3_shared_mem_blocking.cuh
+++ src/sgemm/3_shared_mem_blocking.cuh
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <cuda_rumtime.h>
+#include <cuda_runtime.h>
 #include <cstdlib>
 
-#define CEIL_DIV(M, N) (((M) + (N - 1)) / )
+#define CEIL_DIV(M, N) (((M) + (N - 1)) / (N))
 
 /*
 Matrix Sizes:
@@ -13,7 +13,7 @@ C: MxN
 */
 
 template<const int BLOCK_SIZE>
-__global__ void sgemm_shared_mem_block(int M, int N, int K, float alpha, float *A, float *B, float beta, float *C) {
+__global__ void sgemm_shared_mem_block(int M, int N, int K, float alpha, const float *A, const float *B, float beta, float *C) {
 
     const uint cRow = blockIdx.x;
     const uint cCol = blockIdx.y;
@@ -23,20 +23,21 @@ __global__ void sgemm_shared_mem_block(int M, int N, int K, float alpha, float *
     __shared__ float b_shmem[BLOCK_SIZE * BLOCK_SIZE];
 
     const uint threadRow = threadIdx.x / BLOCK_SIZE;
-    const uint threadCol = threadIdx.y % BLOCK_SIZE;
+    const uint threadCol = threadIdx.x % BLOCK_SIZE;
 
     // move pointers to start position
     A += cRow * BLOCK_SIZE * K;                       // row = cRow, col = 0
     B += cCol * BLOCK_SIZE;                           // row = 0, cCol = cCol
     C += cRow * BLOCK_SIZE * N + cCol * BLOCK_SIZE;   // row = cRow, col = cCol
            
-    float temp = 0.0f;
+    float temp = 0.0;
 
     #pragma unroll 
     for (int blckIdx = 0; blckIdx < K; blckIdx += BLOCK_SIZE) {
 
-        a_shmem[T * BLOCK_SIZE + cCol] = A[blckIdx * M + cCol];
-        b_shmem(blckIdx * BLOCK_SIZE + cCol) = B[blckIdx * N + cCol];
+        // make thread col the consecutive index --> gmem coalescing 
+        a_shmem[threadRow * BLOCK_SIZE + threadCol] = A[threadRow * K + threadCol];
+        b_shmem[threadRow * BLOCK_SIZE + threadCol] = B[threadRow * N + threadCol];
 
         __syncthreads();
 
@@ -45,10 +46,10 @@ __global__ void sgemm_shared_mem_block(int M, int N, int K, float alpha, float *
 
         #pragma unroll 
         for (int dotIdx = 0; dotIdx < BLOCK_SIZE; ++dotIdx) {
-            temp += a_shmem[cRow * BLOCK_SIZE + dotIdx] * b_shmem[dotIdx * BLOCK_SIZE + cCol];
+            temp += a_shmem[threadRow * BLOCK_SIZE + dotIdx] * b_shmem[dotIdx * BLOCK_SIZE + threadCol];
         }
 
         __syncthreads();
     }
-    C[cRow * N + cCol] = temp * alpha + beta * C[cRow * N + cCol];
+    C[threadRow * N + threadCol] = temp * alpha + beta * C[threadRow * N + threadCol];
 }
